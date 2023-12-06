51b5f18e03d74c6f697c2602c361e9420154f8ee
tshepo-ncube <115148619+tshepo-ncube@users.noreply.github.com>
2023-12-06 23:36:38 +0200

    Update head.tsx

--- app/head.tsx
+++ app/head.tsx
@@ -1,23 +1,9 @@
-
 export default function Head() {
   return (
     <>
       <title>Mindful Manifesters</title>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
       <link rel="icon" href="/favicon.ico" />
-
-      {/* Google tag (gtag.js) */}
-      <script async src="https://www.googletagmanager.com/gtag/js?id=G-YE94Z3VF7X"></script>
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-YE94Z3VF7X');
-          `,
-        }}
-      />
     </>
   );
 }
