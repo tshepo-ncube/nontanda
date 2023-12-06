4bf24b93c61bc72c3373c1d2ef8264fe7a1a0bfc
tshepo-ncube <115148619+tshepo-ncube@users.noreply.github.com>
2023-12-06 23:02:36 +0200

    Update head.tsx
    google analytics...

--- app/head.tsx
+++ app/head.tsx
@@ -4,6 +4,16 @@ export default function Head() {
       <title>Mindful Manifesters</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <link rel="icon" href="/favicon.ico" />
+
+      <!-- Google tag (gtag.js) -->
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-YE94Z3VF7X"></script>
+      <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+      
+        gtag('config', 'G-YE94Z3VF7X');
+      </script>
     </>
   );
 }
