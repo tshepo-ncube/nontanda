7e7470dc603a91f92b0e8f2c735cfc7077e2ae26
tshepo-ncube <115148619+tshepo-ncube@users.noreply.github.com>
2023-12-07 10:13:04 +0200

    Update page.tsx

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -28,12 +28,14 @@ const PostPage = (props: any) => {
         <h1 className="text-2xl text-slate-600 ">{post.data.title}</h1>
         <p className="text-slate-400 mt-2">{post.data.date}</p>
       </div>
-
-      <div className="my-12 text-center">
-           <article className="prose">
-              <Markdown>{post.content}</Markdown>
-          </article>
-      </div>
+      <center>
+        <div className="my-12 text-center">
+             <article className="prose">
+                <Markdown>{post.content}</Markdown>
+            </article>
+        </div>
+      </center>
+      
 
      
       <About/>
