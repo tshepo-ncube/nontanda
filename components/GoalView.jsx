746122040335de18d933dc7b46e9693d118ae637
tshepo-ncube <m.tshepo.ncube@gmail.com>
2024-03-01 16:09:46 +0200

    fixed nav

--- components/GoalView.jsx
+++ components/GoalView.jsx
@@ -188,10 +188,10 @@ const GoalView = () => {
 
 const Navbar2 = ({ title }) => {
   return (
-    <nav className="z-14 bg-white p-4 fixed top-0 w-full flex justify-center items-center mt-5">
+    <nav className="z-14 bg-white p-4 fixed top-0 w-full flex justify-center items-center">
       <center>
         <div
-          className="text-black text-3xl  font-bold"
+          className="text-black text-3xl  font-bold pt-4"
           style={{ fontSize: 21 }}
         >
           <h1>{title}</h1>
