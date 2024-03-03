7eab6d9aedb94673262087172fff4bd49b5d0896
tshepo-ncube <m.tshepo.ncube@gmail.com>
2024-03-03 09:52:52 +0200

    form goal img asdded

--- components/GoalForm.jsx
+++ components/GoalForm.jsx
@@ -30,7 +30,7 @@ const GoalForm = ({ onAddGoal }) => {
   return (
     <div style={{ marginTop: 10 }}>
       <ModalNavbar title={"Create A New Goal"} />
-      <div style={{ marginTop: 60, padding: 10 }}>
+      <form style={{ marginTop: 60, padding: 10 }}>
         <label className="block text-sm font-semibold mb-2">
           Goal Name:
           <input
@@ -40,6 +40,7 @@ const GoalForm = ({ onAddGoal }) => {
             value={goal.text}
             onChange={handleChange}
             className="w-full border border-gray-300 p-2 rounded-md mt-2"
+            required
           />
         </label>
         <label className="block text-sm font-semibold mb-2">
@@ -50,30 +51,51 @@ const GoalForm = ({ onAddGoal }) => {
             value={goal.timeline}
             onChange={handleChange}
             className="w-full border border-gray-300 p-2 rounded-md mt-2"
+            required
           />
         </label>
-        <label className="block text-sm font-semibold mb-10">
+        <label className="block text-sm font-semibold mb-2">
           Description:
           <textarea
             name="description"
             placeholder="Tell me more about your goal, HOW you aim to achieve it and WHY you want to achieve it!"
             value={goal.description}
+            required
             onChange={handleChange}
             className="w-full border h-40 resize-none border-gray-300 p-2 rounded-md mt-2"
           />
         </label>
+        <label
+          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          for="file_input"
+        >
+          Image Related To Goal
+        </label>
+        <div className="bg-blue-200 w-full p-4 rounded-lg mb-2 ">
+          <p className="text-black">
+            This is for visualization, the more you see an image to your goal,
+            the more you manifest it!
+          </p>
+        </div>
+
+        <input
+          className="block w-full mb-2 text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+          id="file_input"
+          type="file"
+          required
+        ></input>
 
         <Fab
           color="primary"
           size="medium"
-          className="bg-green-600 hover:bg-green-500 z-0 "
+          className="bg-green-600 hover:bg-green-500 z-0 mt-2"
           variant="extended"
           onClick={handleAddGoal}
         >
           <AddIcon />
           Add Goal
         </Fab>
-      </div>
+      </form>
     </div>
   );
 };
--- components/PostPreview.tsx
+++ components/PostPreview.tsx
@@ -4,59 +4,6 @@ import { PostMetadata } from "./PostMetadata";
 const PostPreview = (props: PostMetadata) => {
   return (
     <>
-      {/* <div className=" mx-auto">
-        <div className="bg-white shadow-md border border-gray-200 rounded-lg w-80 mb-5">
-          <a href="#">
-            <img
-              className="rounded-t-lg"
-              src="https://flowbite.com/docs/images/blog/image-1.jpg"
-              alt=""
-            ></img>
-          </a>
-          <div className="p-5">
-            <a href="#">
-              <h5 className="text-gray-900 font-bold text-2xl tracking-tight mb-2">
-                {props.title}
-              </h5>
-            </a>
-            <p className="font-normal text-gray-700 mb-3">{props.subtitle}</p>
-            <a
-              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 text-center inline-flex items-center"
-              href={`/posts/${props.slug}`}
-            >
-              Read more
-            </a>
-          </div>
-        </div>
-   
-      </div>
-      <div className=" mx-auto">
-        <div className="bg-white shadow-md border border-gray-200 rounded-lg w-80 mb-5">
-          <a href="#">
-            <img
-              className="rounded-t-lg"
-              src="https://flowbite.com/docs/images/blog/image-1.jpg"
-              alt=""
-            ></img>
-          </a>
-          <div className="p-5">
-            <a href="#">
-              <h5 className="text-gray-900 font-bold text-2xl tracking-tight mb-2">
-                {props.title}
-              </h5>
-            </a>
-            <p className="font-normal text-gray-700 mb-3">{props.subtitle}</p>
-            <a
-              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 text-center inline-flex items-center"
-              href={`/posts/${props.slug}`}
-            >
-              Read more
-            </a>
-          </div>
-        </div>
-     
-      </div> */}
-
       <a
         href={`/posts/${props.slug}`}
         className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
@@ -73,34 +20,6 @@ const PostPreview = (props: PostMetadata) => {
           <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
             {props.subtitle}
           </p>
-          {/* 
-          <a
-            className="text-white w-70 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 text-center inline-flex items-center"
-            href={`/posts/${props.slug}`}
-          >
-            Read more
-          </a> */}
-          {/* <a
-            href="#"
-            className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >
-            Read more
-            <svg
-              className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 14 10"
-            >
-              <path
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M1 5h12m0 0L9 1m4 4L9 9"
-              />
-            </svg>
-          </a> */}
         </div>
       </a>
     </>
