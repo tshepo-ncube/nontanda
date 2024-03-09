10d5c7967600f71b4e8a717392c228e0359c02a2
tshepo-ncube <m.tshepo.ncube@gmail.com>
2024-03-09 18:17:47 +0200

    progress modal added

--- components/GoalView.jsx
+++ components/GoalView.jsx
@@ -4,17 +4,37 @@ import Box from "@mui/material/Box";
 import Rating from "@mui/material/Rating";
 import { IconButton } from "@mui/material";
 import Button from "@mui/material/Button";
-import ThumbUpIcon from "@mui/icons-material/ThumbUp";
+import AddIcon from "@mui/icons-material/Add";
+
 import ThumbDownIcon from "@mui/icons-material/ThumbDown";
 import Typography from "@mui/material/Typography";
 import Fab from "@mui/material/Fab";
 import SendIcon from "@mui/icons-material/Send";
 import Navbar from "./Navbar";
 import ModalNavbar from "./ModalNavbar";
+
+import Backdrop from "@mui/material/Backdrop";
+import Modal from "@mui/material/Modal";
+import Fade from "@mui/material/Fade";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: "80%",
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
 // GoalView component
 const GoalView = () => {
   const [value, setValue] = React.useState(2);
   const [newMessage, setNewMessage] = useState("");
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
   const [placeholderText, setPlaceholderText] = useState(
     "In what ways have my habits contributed to or hindered my progress?"
   );
@@ -73,7 +93,7 @@ const GoalView = () => {
     <div className="flex flex-col items-center h-auto w-85 max-w-screen-lg mx-auto overflow-y-auto">
       <ModalNavbar title={" I want to have a consistent routine"} />
 
-      <div style={{ marginTop: 20, userSelect: "none" }}>
+      <div style={{ marginTop: 5, userSelect: "none" }}>
         <div className="bg-blue-200 w-full p-4 rounded-lg mb-4">
           <p className="text-black">
             I want to be consistent in my routine. This includes, sleeping on
@@ -82,121 +102,98 @@ const GoalView = () => {
         </div>
 
         <center>
-          <Button variant="text">view progress</Button>
+          <Button variant="text" onClick={handleOpen}>
+            view progress
+          </Button>
         </center>
-        
+
         <div className="bg-white-200 w-full p-2 pt-2 rounded-lg mb-4 border">
           <div class="flex flex-col">
-            <div className="bg-white p-4">
-              {/* prompts from the AI*/}
-              <div>
-                <p>
-                  The A.I should assess the previous reflection and these
-                  questions are a follow up from the previous reflection session
-                </p>
-                <div
-                  className={`max-w-2/3 p-2 mb-2 ml-4 rounded-lg bg-green-500 text-white float-left`}
-                >
-                  Did you wake up early today?
-                </div>
-                <div
-                  className={`max-w-2/3 p-2 mb-2 ml-4 rounded-lg bg-green-500 text-white float-left`}
-                >
-                  How did you prioritize and manage your time?
-                </div>
-                <div
-                  className={`max-w-2/3 p-2 mb-2 ml-4 rounded-lg bg-green-500 text-white float-left`}
-                >
-                  What challenges did you face in keeping a consitent routine?
-                </div>
+            {/* <div className="bg-white p-2">
+              <div className="bg-white p-2">
+                <div className="bg-white border w-90 p-2 rounded-lg"></div>
               </div>
-            </div>
+            </div> */}
 
-            <div className="bg-white p-2">
-              {/* message from the AI*/}
-              <div className=" bg-green-200  border w-90 p-2 rounded-lg mb-2">
-                <p className="text-black">
-                  I feel good about myself, I am fostering a reslient mindset, I
-                  am trying to be more reslient
-                </p>
-              </div>
-            </div>
-            <div className="bg-white p-2">
-              {/* message from the AI*/}
-              <div className="bg-white border w-90 p-2 rounded-lg">
-                <p className="text-black">
-                  Fostering a resilient mindset, embracing positivity, and
-                  prioritizing mental well-being for a healthier, happier, and
-                  more fulfilling life journey. How are you going to make sure
-                  you remain reslient?
-                </p>
-                <div className="mt-auto">
-                  <IconButton color="blue" style={{ color: "green" }}>
-                    <ThumbUpIcon />
-                  </IconButton>
-                  <IconButton color="white">
-                    <ThumbDownIcon />
-                  </IconButton>
-                </div>
-              </div>
-            </div>
-          </div>
+            <div>
+              <center>
+                <label
+                  style={{ marginBottom: -10 }}
+                  for="email"
+                  className=" block text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Reflect on your journey
+                </label>
+              </center>
+              <div className="flex items-center p-[1.5] mt-4 mb-2 w-full">
+                <textarea
+                  className="flex-1 p-2 h-80 max-h-120 rounded border "
+                  placeholder={placeholderText}
+                  value={newMessage}
+                  rows={calculateRows(newMessage)}
+                  onChange={(e) => setNewMessage(e.target.value)}
+                />
 
-          <div>
-            <center>
-              <label
-                style={{ marginBottom: -10 }}
-                for="email"
-                className=" block text-sm font-medium text-gray-900 dark:text-white"
-              >
-                Reflect on your journey (--ai should be able to ask why you feel
-                this way and go deeper)
-              </label>
-            </center>
-            <div className="flex items-center p-4 w-full">
-              <textarea
-                className="flex-1 p-2 mr-2  rounded border "
-                placeholder={placeholderText}
-                value={newMessage}
-                rows={calculateRows(newMessage)}
-                onChange={(e) => setNewMessage(e.target.value)}
-              />
-
-              {/* <div className="bg-green-700 hover:bg-green-500  rounded-full shadow-md">
+                {/* <div className="bg-green-700 hover:bg-green-500  rounded-full shadow-md">
                 <Fab color="success" size="medium">
                   <SendIcon />
                 </Fab>
               </div> */}
-            </div>
-            <div className="bg-green-700 hover:bg-green-500  rounded-full shadow-md">
-              <Fab
-                color="primary"
-                className="w-full"
-                variant="extended"
-                onClick={() => {
-                  toggleModal("New Goal");
-                }}
-              >
-                <SendIcon className="ml-2 mr-2" />
-                send message
-              </Fab>
-            </div>
-          </div>
+              </div>
+              <button className="bg-green-700 w-full hover:bg-green-500 p-2 rounded-full shadow-md">
+                {/* <Fab
+                  color="primary"
+                  className="w-full"
+                  variant="extended"
+                  onClick={() => {
+                    toggleModal("New Goal");
+                  }}
+                ></Fab> */}
 
-          <center>
-            <Button
-              color="success"
-              variant="text"
-              style={{ backgroundColor: "white" }}
-            >
-              New Reflection
-            </Button>
-            <Button variant="text" style={{ backgroundColor: "white" }}>
-              Reflection On A New Goal
-            </Button>
-          </center>
+                <AddIcon className="justify-center items-center ml-2 mr-2 text-white" />
+              </button>
+            </div>
+
+            <center>
+              <Button
+                color="success"
+                variant="text"
+                style={{ backgroundColor: "white" }}
+              >
+                New Reflection
+              </Button>
+              <Button variant="text" style={{ backgroundColor: "white" }}>
+                Reflection On A New Goal
+              </Button>
+            </center>
+          </div>
         </div>
       </div>
+
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        slots={{ backdrop: Backdrop }}
+        slotProps={{
+          backdrop: {
+            timeout: 500,
+          },
+        }}
+      >
+        <Fade in={open}>
+          <Box sx={style}>
+            <Typography id="transition-modal-title" variant="h6" component="h2">
+              Text in a modal
+            </Typography>
+            <Typography id="transition-modal-description" sx={{ mt: 2 }}>
+              Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+            </Typography>
+          </Box>
+        </Fade>
+      </Modal>
     </div>
   );
 };
