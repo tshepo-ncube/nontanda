11803bf0a99e23aba0eb3a63fbf40bf99c50f0af
tshepo-ncube <m.tshepo.ncube@gmail.com>
2024-03-09 16:35:12 +0200

    link target

--- components/home-components/ReflectionAssistant.jsx
+++ components/home-components/ReflectionAssistant.jsx
@@ -313,7 +313,7 @@ function ReflectionAssistant() {
               clear chat
             </Button>
 
-            <Link href="/goals">
+            <Link href="/goals" target="_blank">
               <Button variant="text" style={{ backgroundColor: "white" }}>
                 Reflect On A New Goal
               </Button>
